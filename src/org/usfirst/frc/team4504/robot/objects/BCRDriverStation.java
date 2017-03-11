4e51481b0f6ff18069161386d4e0d98d466a728c
porgull <connorlbark@gmail.com>
2017-03-11 10:26:21 -0500

    Added driverstation functions

--- src/org/usfirst/frc/team4504/robot/objects/BCRDriverStation.java
+++ src/org/usfirst/frc/team4504/robot/objects/BCRDriverStation.java
@@ -1,11 +1,86 @@
 package org.usfirst.frc.team4504.robot.objects;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class BCRDriverStation extends Joystick
 {
 	public BCRDriverStation(int port)
 	{
 		super(port);
+	}
+	
+	public boolean getTopLeft()
+	{
+		return this.getRawButton(2);
+	}
+	public boolean getTopRight()
+	{
+		return this.getRawButton(4);
+	}
+	public boolean getMidLeft()
+	{
+		return this.getRawButton(10);
+	}
+	public boolean getMidMid()
+	{
+		return this.getRawButton(1);
+	}
+	public boolean getMidRight()
+	{
+		return this.getRawButton(5);
+	}
+	public boolean getBottomLeft()
+	{
+		return this.getRawButton(8);
+	}
+	public boolean getBottomMid()
+	{
+		return this.getRawButton(11);
+	}
+	public boolean getLeverForward()
+	{
+		return this.getRawButton(6);
+	}
+	public boolean getLeverBack()
+	{
+		return this.getRawButton(7);
+	}
+	
+	public JoystickButton getTopLeftButton()
+	{
+		return new JoystickButton(this, 2);
+	}
+	public JoystickButton getTopRightButton()
+	{
+		return new JoystickButton(this, 2);
+	}
+	public JoystickButton getMidLeftButton()
+	{
+		return new JoystickButton(this, 2);
+	}
+	public JoystickButton getMidMidButton()
+	{
+		return new JoystickButton(this, 2);
+	}
+	public JoystickButton getMidRightButton()
+	{
+		return new JoystickButton(this, 2);
+	}
+	public JoystickButton getBottomLeftButton()
+	{
+		return new JoystickButton(this, 2);
+	}
+	public JoystickButton getBottomRightButton()
+	{
+		return new JoystickButton(this, 2);
+	}
+	public JoystickButton getLeverForwardButton()
+	{
+		return new JoystickButton(this, 2);
+	}
+	public JoystickButton getLeverBackButton()
+	{
+		return new JoystickButton(this, 2);
 	}
 }
